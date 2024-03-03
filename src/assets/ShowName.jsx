4ce3bce0d4ac6fc1361f64a98ea048a68f5ceb53
Anirach Mingkhwan <anirach.m@fitm.kmutnb.ac.th>
2024-03-03 13:00:32 +0700

    .done code

--- src/assets/ShowName.jsx
+++ src/assets/ShowName.jsx
@@ -1,17 +1,13 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-export default function ShowName({name, show}) {
-console.log('name:', name)
-console.log('show:', show);
-    return (
-        <div>
-            {show && <h2>Your name is: {name}</h2>}
-        </div>
-    )
+export default function ShowName({ name, show }) {
+  console.log("name:", name);
+  console.log("show:", show);
+
+  return <div>{show && <h2>Your name is: {name}</h2>}</div>;
 }
 
 ShowName.propTypes = {
-    name: PropTypes.string.isRequired,
-    show: PropTypes.bool.isRequired
-}
-
+  name: PropTypes.string.isRequired,
+  show: PropTypes.bool.isRequired,
+};
